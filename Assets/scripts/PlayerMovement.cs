331a2515630fbba754cb900b5f20df9c69cc1dfc
ArisingCnightE <raoul.heurneman@gmail.com>
2024-03-11 22:30:23 +0100

    comments added

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
-
     public Animator animator;
-
     Vector2 movement; 
 
-    // Update is called once per frame
     void Update()
-    {
+    {   //hier word gekeken welke knop er wordt ingedrukt en als het of de pijltjes of WASD is wordt er een x of y aan toegevoegd
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        //animator aangeven dat de x en y waardes ook gelijk zijn aan horizontale en verticale beweging. 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
+        //als één van de knopjes word ingedrukt dan heeft de player "movespeed" en dus wordt hij verandert 
+        //van de idle sprite naar de rennende sprite
         animator.SetFloat("speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
+        //hier wordt de player sprite ook echt verplaatst van plek op het scherm
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
